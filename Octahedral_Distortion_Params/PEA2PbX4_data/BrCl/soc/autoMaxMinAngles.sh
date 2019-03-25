dd3b3ca566d830b1aa19734a04c436bb1c609871
Alan Liu <alan1997liu@berkeley.edu>
2019-03-25 12:38:35 -0700

    03_25_19 update

---
+++ Octahedral_Distortion_Params/PEA2PbX4_data/BrCl/soc/autoMaxMinAngles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Make sure you specify the B atom of interest when you run the script.
+echo Enter the B atom of interest: 
+read B_atom
+for file in *.vasp; do
+	halide_atom=`echo ${file} | cut -d"_" -f1`
+	./maxMinDistortionAngles.py ${file} $B_atom $halide_atom
+done
+
--- Octahedral_Distortion_Params/scripts/calcMaxMinAngles/maxMinDistortionAngles.py
+++ Octahedral_Distortion_Params/scripts/calcMaxMinAngles/maxMinDistortionAngles.py
@@ -285,3 +285,4 @@ center_octahedrals = get_center_octahedrals(octahedral_array, numUnitCells)
 
 get_distortion_info(center_octahedrals, octahedral_array, struct)
 
+
